fc87d1ced2b522021ad05d15df3ffad8e4c59b69
Prosper <nkonyeprospero@gmail.com>
2022-12-09 00:03:22 +0100

    just started working

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { ethers, BigNumber } = require('ethers');
 const ERC20ABI = require('./abi.json');
 const Flutterwave = require('flutterwave-node-v3');
 const got = require("got");
+const bitcore = require("bitcore-lib");
+const axios = require("axios");
 
 
 
@@ -26,6 +28,7 @@ const INFURA_URL_TESTNETbtc = process.env.INFURA_URL_TESTNETbtc;
 
 
 
+let returnValue = false;
 
 
 
@@ -35,6 +38,15 @@ app.get("/",  function(req, res){
 
 
 
+
+  //get return value
+  app.get("/check",  function(req, res){
+    res.send({check: returnValue});
+  });
+
+
+//main link https://blok-ramp.herokuapp.com/payment-callback
+//test link  http://localhost:8000/payment-callback
 app.post('/paymentlink', async (req, res) => {
     console.log(req.body);
       try {
@@ -46,7 +58,7 @@ app.post('/paymentlink', async (req, res) => {
                 tx_ref: req.body.tx,
                 amount: req.body.amount,
                 currency: "USD",
-                redirect_url: "https://blok-ramp.herokuapp.com/payment-callback",
+                redirect_url: "http://localhost:8000/payment-callback",
 
                 customer: { 
                   email: req.body.email,
@@ -98,6 +110,11 @@ app.post('/paymentlink', async (req, res) => {
                 // Sending ether
                 await walletget.sendTransaction(tx);
 
+               //res.redirect(`http://127.0.0.1:5173/${req.query.tx_ref}`);
+              //const done = await got(`http://127.0.0.1:5173/${req.query.tx_ref}`, { json: true });
+              //res.send(done)
+              returnValue = true;
+
             }  else if(value[1] === "binancecoin") {
 
                 const provider = new ethers.providers.JsonRpcProvider(INFURA_URL_TESTNETbnb);
@@ -116,27 +133,111 @@ app.post('/paymentlink', async (req, res) => {
                 // Sending ether
                 await walletget.sendTransaction(tx);
 
+                //res.redirect(`http://127.0.0.1:5173/${req.query.tx_ref}`);
+                //const done = await got(`http://127.0.0.1:5173/${req.query.tx_ref}`, { json: true });
+                //res.send(done)
+                returnValue = true;
+
             } else if(value[1] === "bitcoin") {
+            
+              const sochain_network = "BTCTEST"; // the Testnet network for sochain
 
-                const provider = new ethers.providers.JsonRpcProvider(INFURA_URL_TESTNETbtc);
+              /* your bitcoin address. The one you want to send funds from -- the one we just generated */
+              const sourceAddress = `mpyEsPc1YFjrxTxryXAwpNSw3TFq4URQDx`;
 
-                tx = {
-                    to: value[2],
-                    value: ethers.utils.parseEther(value[3])
+              /**
+              because the outputs come in satoshis, and 1 Bitcoin is equal to 100,000,000 satoshies, we'll multiply the amount of bitcoin by 100,000,000 to get the value in satoshis.
+              */
+                const satoshiToSend = value[3] * 100000000;
+                console.log(satoshiToSend)
+                let fee = 0; 
+                let inputCount = 0;
+                let outputCount = 2; // we are going to use 2 as the output count because we'll only send the bitcoin to 2 addresses the receiver's address and our change address.
+
+
+
+                //const utxos = await got(`https://sochain.com/api/v2/get_tx_unspent/${sochain_network}/${sourceAddress}`);
+                //console.log(await utxos.body), "one";
+                const utxos = await axios.get(
+                  `https://sochain.com/api/v2/get_tx_unspent/${sochain_network}/${sourceAddress}`,
+                  {
+                    headers: {
+                      'Accept-Encoding': 'application/json',
+                    }
+
                   }
-     
-                //instantiate wallet
-                const walletget = new ethers.Wallet( process.env.PRIVATEKEY, provider)
-    
-                  // Signing a transaction
-                await walletget.signTransaction(tx)
-    
-                // Sending ether
-                await walletget.sendTransaction(tx)
+                );
+                console.log(await utxos.data.data.txs, "one");
+                
+                let waitwait = await utxos.data.data.txs;
+
+                const transaction = new bitcore.Transaction();
+                let totalAmountAvailable = 0;
+                let inputs = [];
+                //let utxos = response.data.data.txs;
+             
+                for (const element of waitwait) {
+                  let utxo = {};
+                  utxo.satoshis = Math.floor(Number(element.value) * 100000000);
+                  utxo.script = element.script_hex;
+                  utxo.address = utxos.data.data.address;
+                  utxo.txId = element.txid;
+                  utxo.outputIndex = element.output_no;
+                  totalAmountAvailable += utxo.satoshis;
+                  inputCount += 1;
+                  inputs.push(utxo);
+                }
+
+                console.log(totalAmountAvailable);
+
+                const transactionSize = inputCount * 146 + outputCount * 34 + 10 - inputCount;
+                // Check if we have enough funds to cover the transaction and the fees assuming we want to pay 20 satoshis per byte
+
+                fee = transactionSize * 20
+                if (totalAmountAvailable - satoshiToSend - fee  <= 0) {
+                  throw new Error("Balance is too low for this transaction");
+                }
+
+                
+                //Set transaction input
+                transaction.from(inputs);
+
+                // set the recieving address and the amount to send
+                transaction.to(value[2], satoshiToSend);
+
+                // Set change address - Address to receive the left over funds after transfer
+                transaction.change(sourceAddress);
+
+                //manually set transaction fees: 20 satoshis per byte
+                transaction.fee(fee);
+
+                // Sign transaction with your private key
+                transaction.sign(process.env.PRIVATEKEYBTC);
+
+                // serialize Transactions
+                const serializedTransaction = transaction.serialize();
+                
+                // Send transaction
+                const response = await got.post(`https://sochain.com/api/v2/send_tx/${sochain_network}`, {
+                  json: {
+                      tx_hex: serializedTransaction,
+                  }
+
+              }).json();
+
+              console.log(response.data.data);
+              returnValue = true;
+
+              return response.data.data;
+              
+
+              //res.redirect(`http://127.0.0.1:5173/${req.query.tx_ref}`);
+              //const done = await got(`http://127.0.0.1:5173/${req.query.tx_ref}`);
+              //res.redirect(`http://127.0.0.1:5173/${req.query.tx_ref}`);
+
+
             }
 
-
-            res.send("Worked");
 
 
       } 
